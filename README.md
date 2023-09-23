759be3189945558049537831f09cb8aabae15b71
danikagiron <danikag@kth.se>
2023-09-23 13:20:27 +0800

    README finished, project finished?

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # first-p5js
 TESTING OUT p5.js
 
-Hello I'm just trying out p5.js in order to experiment with code and art
-Also to just have a refresher on using vscode and github in general
+Hello I'm just trying out p5.js in order to experiment with code and art. Also I want to just have a refresher on using vscode and github in general.
+
+In this p5.js file just click around to see different shapes.
--- sketch.js
+++ sketch.js
@@ -1,12 +1,49 @@
+
+const colour = 0;
+
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(displayWidth, displayHeight);
+  background(249, 235, 245); // pink
+  textFont('Courier New', 28);
+  fill(0);
+  textAlign(LEFT);
+  text('Click around to see different shapes!', 20, 50);
+
+  
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(140, 173, 217);
-  } else {
-    fill(145, 140, 217);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+ 
+}
+  
+
+function mouseClicked() {
+  const r = int(random(1,5));
+    // text(r, mouseX, mouseY, [500], [500]);
+  
+    switch (r) {
+      
+      case 1:
+        fill(140, 173, 217); 
+        ellipse(mouseX, mouseY, 80, 80);
+        break;
+      case 2:
+        fill(145, 140, 217); 
+        rect(mouseX, mouseY, 80, 80);
+        break;
+      case 3:
+        fill(186, 44, 134); 
+        triangle(mouseX, mouseY-40, mouseX, mouseY+40, mouseX+80 , mouseY+40);
+        break;
+      case 4:
+        fill(75, 141, 185); 
+        triangle(mouseX, mouseY-40, mouseX, mouseY+40, mouseX-80 , mouseY+40);
+        break;
+      default:
+        fill(140, 173, 217); 
+        ellipse(mouseX, mouseY, 80, 80);
+        break;
+    }
+
+  return false;
 }
